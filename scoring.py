a4b5b1c9a5fe1a3861f4924b9da17f96bb687bf9
raymondlaghaeian <raymondl@microsoft.com>
2017-07-07 16:22:42 +0000

    Update

--- scoring.py
+++ scoring.py
@@ -2,12 +2,10 @@
 # init() and run() functions. 
 def init():
     import os
+    #Set backend to tensorflow
     os.environ['KERAS_BACKEND']='tensorflow'
-
-    # read in the model file
     from keras.models import Sequential
     from keras.layers import Dense
-    import numpy
     from keras.models import load_model
     
     # load the model
@@ -16,7 +14,6 @@ def init():
     
 def run(inputstring):
     import numpy
-    #input1 = numpy.array([[1.,85.,66.,29.,0.,26.6,0.351,31.]])
     input1 = numpy.fromstring(inputstring,dtype=float, sep=',').reshape((1,8))
     score=trainedmodel.predict(input1)
     return str(score[0][0])
